1bb480494bc3bbddf844b3c0ea4bc05b9dda946b
RodrigoDiasDeOliveira <rodrigodigau02@gmail.com>
2024-10-23 18:27:05 +0200

    one more time..

--- internxt-frontend/src/test/FilePanel.test.tsx
+++ internxt-frontend/src/test/FilePanel.test.tsx
@@ -4,15 +4,3 @@ import { AuthContext } from '../components/context/AuthContext';
 import FilePanel from '../components/FilePanel/FilePanel';
 import '@testing-library/jest-dom/extend-expect';
 
-test('displays no files message when file list is empty', async () => {
-  render(
-    <AuthContext.Provider value={{ isAuthenticated: true }}>
-      <FilePanel />
-    </AuthContext.Provider>
-  );
-
-  // Simula uma resposta vazia de arquivos
-
-  await waitFor(() => expect(screen.getByText(/nenhum arquivo encontrado/i)).toBeInTheDocument());
-});
-
