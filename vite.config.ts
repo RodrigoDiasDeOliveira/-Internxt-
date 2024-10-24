fcf8374730b63b82e1caea8821fd56b5872d60a9
RodrigoDiasDeOliveira <rodrigodigau02@gmail.com>
2024-10-24 12:59:34 +0200

    seven.three

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '../internxt-frontend/src', // Se o seu index.html estiver na pasta src, ajuste conforme necessário
+  root: '../internxt-frontend', // Se o seu index.html estiver na pasta src, ajuste conforme necessário
   build: {
     outDir: '../dist', // O diretório de saída para a build
   },
